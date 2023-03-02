ec12aa498290d55419a2abfb1a849a94010cd1a5
Krushnar567 <115181197+Krushnar567@users.noreply.github.com>
2023-03-02 23:25:42 +0530

    Update HomePage.java

--- WebMathProject/WebMathProject/src/test/java/webMathProject/HomePage.java
+++ WebMathProject/WebMathProject/src/test/java/webMathProject/HomePage.java
@@ -24,7 +24,7 @@ import org.testng.annotations.AfterClass;
  @Test
  public void homePage() throws IOException {
 	  
-	    WebElement generalMath = driver.findElement(By.xpath(p.getProperty("generalMath")));
+	WebElement generalMath = driver.findElement(By.xpath(p.getProperty("generalMath")));
      
         generalMath.click();
         
@@ -38,26 +38,26 @@ import org.testng.annotations.AfterClass;
  public void launchURL() throws IOException {
 	  
 	  
-	    WebDriverManager.chromedriver().setup();
+	 WebDriverManager.chromedriver().setup();
 
-		driver = new ChromeDriver();
+         driver = new ChromeDriver();
 		
-		FileInputStream fis = new FileInputStream("C:\\Users\\krush\\OneDrive\\Desktop\\Excelr\\WebMathProject\\src\\test\\java\\com\\utils\\data.properties");
+	FileInputStream fis = new FileInputStream("C:\\Users\\krush\\OneDrive\\Desktop\\Excelr\\WebMathProject\\src\\test\\java\\com\\utils\\data.properties");
 	    
-	    p.load(fis);
+	p.load(fis);
 	    
-	    driver.get(p.getProperty("url"));
-		driver.manage().window().maximize();
+	driver.get(p.getProperty("url"));
+        driver.manage().window().maximize();
 		
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
 	  
 	}
 
  @AfterClass
  public void closeBrowser() {
 	  
-	    driver.close();
+	driver.close();
 	  
 	  
   }
